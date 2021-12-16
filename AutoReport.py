d7e76982aa7cd4b6f68c4c9de4765b9b4bef92ee
dayutou <dayutou@2dfire.com>
2021-12-16 13:13:56 +0800

    去除多余配置、编写md

--- AutoReport.py
+++ AutoReport.py
@@ -11,14 +11,6 @@ def auto_report(data):
     userName = data.get("userName")
     passwd = data.get("passwd")
     nickName = data.get("nickName")
-    logging.basicConfig(level=logging.INFO,  # 控制台打印的日志级别
-                        filename='report.log',
-                        filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
-                        # a是追加模式，默认如果不写的话，就是追加模式
-                        format=
-                        '%(asctime)s - %(levelname)s: %(message)s'
-                        # 日志格式
-                        )
     nickName = '[' + nickName + ']'
     bor = webdriver.Edge()
     bor.get("http://zyt.zjnu.edu.cn/H5/Login.aspx?op=phone_html5")
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # auto_report_zyt
 战役通打卡
+
+- 需要提前edge webdrive
+- 需要自己改动run.bat和start.bat的路径（相信聪明的你肯定能够搞定）
+
+## AutoReport.py
+
+利用Selenium模拟人为点击填写上报内容
+
+## main.py
+
+使用线程池提高性能、日志输出到当前目录的report.log中
+
+## run.bat
+
+使用windows的schtasks设置每天定时自动执行
+
+## start.bat
+
+执行py文件
