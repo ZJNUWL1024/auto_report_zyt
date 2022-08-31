2d6275597facb044297d9775afffddefd829371e
dayutou <dayutou@2dfire.com>
2022-08-31 13:00:48 +0800

    修改日志地址

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def process(data_arr):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,  # 控制台打印的日志级别
-                        filename='D:\\logs\\report.log',
+                        filename='./report.log',
                         filemode='a',  ##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
                         # a是追加模式，默认如果不写的话，就是追加模式
                         format=
